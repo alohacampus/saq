8334c89f54f3366ded2556b6de964e484ed84bc6
floah <floah@WIN-HSH>
2019-08-16 14:40:56 +0900

    boj[2606] 바이러스 - dfs 풀이

--- swsaq/src/boj/dfsbfs/example/_2606_dfs.java
+++ swsaq/src/boj/dfsbfs/example/_2606_dfs.java
@@ -1,10 +1,14 @@
 package boj.dfsbfs.example;
+
+
+
+
 /*
 	Q.바이러스
 	신종 바이러스인 웜 바이러스는 네트워크를 통해 전파된다. 한 컴퓨터가 웜 바이러스에 걸리면 
 	그 컴퓨터와 네트워크 상에서 연결되어 있는 모든 컴퓨터는 웜 바이러스에 걸리게 된다.
 	예를 들어 7대의 컴퓨터가 <그림 1>과 같이 네트워크 상에서 연결되어 있다고 하자. 
-	1번 컴퓨터가 웜 바이러스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어
+	1번 컴퓨터가 웜 바이러ㅊ스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어
 	 2, 3, 5, 6 네 대의 컴퓨터는 웜 바이러스에 걸리게 된다. 하지만 4번과 7번 컴퓨터는 1번 컴퓨터와 
 	 네트워크상에서 연결되어 있지 않기 때문에 영향을 받지 않는다.
 	 
@@ -37,6 +41,55 @@ package boj.dfsbfs.example;
 	(출력예시)
 	4
 */
-public class _2606 {
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class _2606_dfs {
+
+	// N : 컴퓨터 수(정점의 수), M : 컴퓨터 쌍의 수(간선의 수)
+	// A : 출발지, B : 목적지
+	static int N, M, A, B;
+	static int [][] MAT = new int[101][101];
+	static ArrayList<Integer> alist[] = new ArrayList[101];
+	static int [] visited = new int[101];
+	static int cnt;
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		// 첫 번째 바이러스 컴퓨터를 통해 바이러스에 걸린 컴퓨터의 수 
+		cnt = 0;
+		for (int i = 1; i <= M; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			MAT[A][B] = MAT[B][A] = 1;
+		}
+		
+		
+		dfs(1);
+		System.out.println(cnt);
+		
+	}
+    
+    public static void dfs(int idx) {
+		// 종료조건 - 전체순회
+		
+		visited[idx] = 1;
+		
+		// 탐색조건
+		for (int i = 1; i <= N; i++) {
+			// idx : 현재 정점, i : 방문하려고 하는 정점
+			// MAT[idx][i] == 1 : 현재 정점에서 i번으로 간선 연결되어 있다면
+			// visited[i] == 0  : 방문한 적이 없다면
+			if( MAT[idx][i] == 1 && visited[i] == 0 ) {
+				cnt++;
+				dfs(i);
+			}
+		}
+		
+	}
 
 }
