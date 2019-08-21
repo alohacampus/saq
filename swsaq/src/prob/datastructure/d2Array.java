c2438212bd98468e5f74cf93c98d81b07c4d2600
floah <floah@WIN-HSH>
2019-08-21 21:55:32 +0900

    배열

--- swsaq/src/prob/datastructure/d2Array.java
+++ swsaq/src/prob/datastructure/d2Array.java
@@ -16,6 +16,8 @@ public class d2Array {
 		arr[2][0] = 31;
 		arr[2][1] = 32;
 		
+		
+		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
 				System.out.print(arr[i][j] + " ");
