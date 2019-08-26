389ab534f5cb3efabf571e51654a3eb3eef1a247
floah <floah@WIN-HSH>
2019-08-26 12:02:27 +0900

    boj[10026] 적록색약 dfs

---
+++ swsaq/src/boj/pre/exam/_10026/_10026_3.java
@@ -0,0 +1,116 @@
+package boj.pre.exam._10026;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class _10026_3 {
+	// N
+	static int N;
+	static String [][] MAT = new String[101][101];
+	static int [][] visited = new int[101][101];
+	static int [] dr = {0,1,0,-1};
+	static int [] dc = {1,0,-1,0};
+	static int cnt, cnt2;  			// 적록색약X, 적록색약O
+	
+	
+	public static void dfs(int x, int y) {
+		
+		String nowRGB = MAT[x][y];
+		// 종료조건
+		// 탐색조건
+		for (int i = 0; i < 4; i++) {
+			int nxt_row = x + dr[i];
+			int nxt_col = y + dc[i];
+			// 격자안에 있는 지 확인
+			if( 1 <= nxt_row && nxt_row <= N &&
+					1 <= nxt_col && nxt_col <= N ) {
+				if( visited[nxt_row][nxt_col] == 0 &&
+						MAT[nxt_row][nxt_col].equals(nowRGB) ) {
+					visited[nxt_row][nxt_col] = 1;
+					dfs(nxt_row, nxt_col);
+				}
+			}
+			
+		}
+		
+		
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+//		Scanner sc = new Scanner(System.in);
+		N = Integer.parseInt(st.nextToken());
+
+		
+		
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String [] str = st.nextToken().split("");
+			for (int j = 0; j < N; j++) {
+				MAT[i][j+1] = str[j];
+			}
+		}
+		
+		
+		
+		// 1. 적록색약이 아닌 경우, 구역의 개수를 센다.
+		// 탐색 - 한구역을 탐색할 때 cnt++
+		for (int i = 0; i < N*N; i++) {
+			// 다음 탐색할 좌표를 구한다.
+			int nxt_row = (i / N) + 1;
+			int nxt_col = (i % N) + 1;
+			if( visited[nxt_row][nxt_col] == 0 ) {
+					cnt++;
+					dfs(nxt_row, nxt_col);
+			}
+		}
+		
+		
+
+		// 초기화 - visited 초기화
+		visited = new int[101][101];
+		
+		
+		// 2. 적록색약인 경우, 구역의 개수를 센다.
+		// G -> R 로 수정
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if( MAT[i][j].equals("G") ) 
+					MAT[i][j] = "R";
+			}
+		}
+		
+		for (int i = 0; i < N*N; i++) {
+			// 다음 탐색할 좌표를 구한다.
+			int nxt_row = (i / N) + 1;
+			int nxt_col = (i % N) + 1;
+			if( visited[nxt_row][nxt_col] == 0 ) {
+					cnt2++;
+					dfs(nxt_row, nxt_col);
+			}
+		}
+		
+		
+//		System.out.println(cnt + " " + cnt2);
+		bw.write(cnt + " " + cnt2);
+		bw.flush();
+		
+	}
+}
+
+
+
+
+
+
+
+
+
